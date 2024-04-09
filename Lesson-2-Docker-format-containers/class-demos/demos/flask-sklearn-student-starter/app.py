34cac76f92e2184bdad6b2cc7f81dfe121fa6fb9
EC2 Default User <ec2-user@ip-172-31-74-21.ec2.internal>
2024-04-09 16:47:26 +0000

    Adding docker proj

--- Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/app.py
+++ Lesson-2-Docker-format-containers/class-demos/demos/flask-sklearn-student-starter/app.py
@@ -1,69 +1,79 @@
-from flask import Flask, request, jsonify
-from flask.logging import create_logger
-import logging
+import click
 
-import pandas as pd
-from sklearn.externals import joblib
-from sklearn.preprocessing import StandardScaler
+@click.command()
+def hello():
+    click.echo('Hello World')
 
-app = Flask(__name__)
-LOG = create_logger(app)
-LOG.setLevel(logging.INFO)
+if __name__ == '__main__':
+    hello()
 
-def scale(payload):
-    """Scales Payload"""
+# from flask import Flask, request, jsonify
+# from flask.logging import create_logger
+# import logging
 
-    LOG.info(f"Scaling Payload: {payload}")
-    scaler = StandardScaler().fit(payload)
-    scaled_adhoc_predict = scaler.transform(payload)
-    return scaled_adhoc_predict
+# import pandas as pd
+# from sklearn.externals import joblib
+# from sklearn.preprocessing import StandardScaler
 
-@app.route("/")
-def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+# app = Flask(__name__)
+# LOG = create_logger(app)
+# LOG.setLevel(logging.INFO)
 
+# def scale(payload):
+#     """Scales Payload"""
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    """Performs an sklearn prediction
+#     LOG.info(f"Scaling Payload: {payload}")
+#     scaler = StandardScaler().fit(payload)
+#     scaled_adhoc_predict = scaler.transform(payload)
+#     return scaled_adhoc_predict
 
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
-
-    result looks like:
-    { "prediction": [ 20.35373177134412 ] }
-
-    """
+# @app.route("/")
+# def home():
+#     html = f"<h3>Sklearn Prediction Home</h3>"
+#     return html.format(format)
 
 
-    json_payload = request.json
-    LOG.info(f"JSON payload: {json_payload}")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"inference payload DataFrame: {inference_payload}")
-    scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
-    LOG.info(f"prediction {prediction}")
-    return jsonify({'prediction': prediction})
+# @app.route("/predict", methods=['POST'])
+# def predict():
+#     """Performs an sklearn prediction
 
-if __name__ == "__main__":
-    clf = joblib.load("boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True)
+#     input looks like:
+#             {
+#     "CHAS":{
+#       "0":0
+#     },
+#     "RM":{
+#       "0":6.575
+#     },
+#     "TAX":{
+#       "0":296.0
+#     },
+#     "PTRATIO":{
+#       "0":15.3
+#     },
+#     "B":{
+#       "0":396.9
+#     },
+#     "LSTAT":{
+#       "0":4.98
+#     }
+
+#     result looks like:
+#     { "prediction": [ 20.35373177134412 ] }
+
+#     """
+
+
+#     json_payload = request.json
+#     LOG.info(f"JSON payload: {json_payload}")
+#     inference_payload = pd.DataFrame(json_payload)
+#     LOG.info(f"inference payload DataFrame: {inference_payload}")
+#     scaled_payload = scale(inference_payload)
+#     prediction = list(clf.predict(scaled_payload))
+#     LOG.info(f"prediction {prediction}")
+#     return jsonify({'prediction': prediction})
+
+# if __name__ == "__main__":
+#     clf = joblib.load("boston_housing_prediction.joblib")
+#     app.run(host='0.0.0.0', port=80, debug=True)
+
